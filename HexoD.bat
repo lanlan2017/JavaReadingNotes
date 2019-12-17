cb1ef3e5e0eca547b9fd65db242abe56a5d68353
lanlan2017 <18251956727@163.com>
2019-12-17 17:52:39 +0800

    更新网站目录

--- HexoD.bat
+++ HexoD.bat
@@ -1,7 +1,7 @@
 @echo off
 echo 切换盘符 %~d0
 %~d0
-echo 进入当前目录 %~dp0source\_posts\
+echo 进入文章目录: %~dp0source\_posts\
 cd %~dp0source\_posts\
 echo.
 echo ############################################################### 删除所有的html文件 开始
@@ -13,7 +13,7 @@ for /r . %%i in (*.html) do (
 echo.
 echo ############################################################### 删除所有的html文件 结束
 echo 生成自定义目录
-java -jar "E:\dev2\idea_workspace\HexoTools\runable\FM.jar" toc %~dp0source\_posts\
-echo 打开 git-bash
+java -jar "E:\dev2\idea_workspace\MyJavaTools\runable\FM.jar" toc %~dp0source\_posts\
+echo 在站点目录:%~dp0 打开git-bash
 start git-bash --cd=%~dp0
 REM pause
